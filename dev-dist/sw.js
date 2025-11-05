13caf56480c6cc574b45a8b745648e9b71411942
Tom Willis <tomwillis@Toms-MacBook-Pro.local>
2025-11-05 19:54:40 +0000

    implemented thread replies in the chat room - updated css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gt3c6ig1h3"
+    "revision": "0.osf04bufqs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
